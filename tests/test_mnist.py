9dd8e0f2cbfc4888f7546b424dcb18e3f479c5c7
showa_yojyo <yojyo@hotmail.com>
2019-11-26 12:11:29 +0900

    (tests) Use TestCase.subTest

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -28,27 +28,31 @@ class MnistTestCase(TestCase):
     def test_load_mnist_normalize(self):
         traint, testt = load_mnist(normalize=True)
         for i in (traint[0], testt[0]):
-            self.assertEqual(i.dtype, np.float32)
-            self.assertTrue(((0 <= i) & (i <= 1.0)).all())
+            with self.subTest(i=i):
+                self.assertEqual(i.dtype, np.float32)
+                self.assertTrue(((0 <= i) & (i <= 1.0)).all())
 
         trainf, testf = load_mnist(normalize=False)
         for i in (trainf[0], testf[0]):
-            self.assertEqual(i.dtype, np.uint8)
+            with self.subTest(i=i):
+                self.assertEqual(i.dtype, np.uint8)
 
     def test_load_mnist_one_hot_label(self):
         traint, testt = load_mnist(one_hot_label=True)
         self.assertEqual(traint[1].shape, (self.ntrain, self.ndigits))
         self.assertEqual(testt[1].shape, (self.ntest, self.ndigits))
         for i in (traint[1], testt[1]):
-            self.assertEqual(i.dtype, np.bool)
-            self.assertTrue(((i == 0) | (i == 1)).all())
+            with self.subTest(i=i):
+                self.assertEqual(i.dtype, np.bool)
+                self.assertTrue(((i == 0) | (i == 1)).all())
 
         trainf, testf = load_mnist(one_hot_label=False)
         self.assertEqual(trainf[1].shape, (self.ntrain,))
         self.assertEqual(testf[1].shape, (self.ntest,))
         for i in (trainf[1], testf[1]):
-            self.assertEqual(i.dtype, np.uint8)
-            self.assertTrue(((i != 0) & (i != 1)).any())
+            with self.subTest(i=i):
+                self.assertEqual(i.dtype, np.uint8)
+                self.assertTrue(((i != 0) & (i != 1)).any())
 
 if __name__ == '__main__':
     main()
